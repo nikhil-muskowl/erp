ee1159333aacbbb3a90e8fe1ba9bb791ad48de65
nikhil-muskowl <nikhil.muskowl@gmail.com>
2018-11-22 17:38:59 +0530

    ff

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  constructor(public alertController: AlertController) {}
+
+  doRefresh(event) {
+    console.log('Begin async operation');
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      event.target.complete();
+    }, 2000);
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  goToSetting(){
+    this.presentAlert();
+    // alert('setting');
+  }
 }
